1cfe9a8b07b4f3f85377533668199a842c452c31
henrytk <henry.knott@digital.cabinet-office.gov.uk>
2017-06-09 13:26:44 +0100

    Implement real API calls
    This provides the first proper broker implementation which uses the
    Compose client to interact with the Compose API.
    
    Despite the `update` operation being implemented this functionality
    is not technically supported, as we only have one plan available
    and we do not have any user provided parameters. Upcoming tests
    will assert that updates are not supported.
    
    We make use of the `operation` query field from the Cloud Foundry
    services API[1]. We set a string value on every provision, deprovision,
    and update operation containing JSON. This JSON describes the operation
    type and recipe ID which can be used to check on the status of the
    operation using the Compose API.
    
    However, a bug was encountered in which the Compose API returns a
    401 when GETing the recipe for a deleted deployment. Compose have
    recognised this and have it tracked as a bug. The workaround for
    now is to assume delete operations were successful.
    
    We return a single credentials string on bind operations as this
    is an established convention and some buildpacks are configured
    to use such URIs and place them in a DATABASE_URL environment
    variable automatically.
    
    [1] https://docs.cloudfoundry.org/services/api.html#polling

---
+++ broker/utils.go
@@ -0,0 +1,38 @@
+package broker
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/alphagov/paas-compose-broker/compose"
+	composeapi "github.com/compose/gocomposeapi"
+)
+
+func findDeployment(compose compose.Client, name string) (*composeapi.Deployment, error) {
+	deployments, errs := compose.GetDeployments()
+	if len(errs) > 0 {
+		return nil, squashErrors(errs)
+	}
+
+	for _, deployment := range *deployments {
+		if deployment.Name == name {
+			return &deployment, nil
+		}
+	}
+
+	return nil, fmt.Errorf("deployment: not found")
+}
+
+func squashErrors(errs []error) error {
+	var s []string
+
+	for _, err := range errs {
+		s = append(s, err.Error())
+	}
+
+	return fmt.Errorf("%s", strings.Join(s, "; "))
+}
+
+func JDBCURI(scheme, hostname, port, dbname, username, password string) string {
+	return fmt.Sprintf("jdbc:mongodb://%s:%s/%s?user=%s&password=%s", hostname, port, dbname, username, password)
+}
